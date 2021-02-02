4aa4cc0dec21a9a05773c924e6fee9edfe0ab9fc
Yasser <yasser@nervelife.com>
2021-02-02 19:22:51 +0000

    Setup Development Environment for Java

---
+++ src/main/java/com/nervelife/springbootgcpjava/SpringBootGcpJavaApplication.java
@@ -0,0 +1,13 @@
+package com.nervelife.springbootgcpjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGcpJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nervelife/springbootgcpjava/SpringBootGcpJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nervelife.springbootgcpjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGcpJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
